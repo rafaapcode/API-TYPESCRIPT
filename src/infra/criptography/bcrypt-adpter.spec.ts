3e04fb95d035efcc50f79f46d550ec9908e6a633
rafaapcode <rafaap2003@gmail.com>
2022-11-23 13:17:11 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adpter.spec.ts
+++ src/infra/criptography/bcrypt-adpter.spec.ts
@@ -1,6 +1,12 @@
 import BcryptAdapter from './bcrypt-adapter'
 import bcryptjs from 'bcryptjs'
 
+jest.mock('bcryptjs', () => ({
+  async hash (): Promise<string> {
+    return Promise.resolve('hash')
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should BcryptAdapter call bcrypt with correct values', async () => {
     const salt = 12
@@ -9,5 +15,13 @@ describe('Bcrypt Adapter', () => {
 
     await sut.encrypt('any_value')
     expect(spyBcrypt).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hash')
   })
 })
