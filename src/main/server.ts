a8864a1078b52e569925b25b5ccaa2e255909b32
rafaapcode <rafaap2003@gmail.com>
2022-11-29 13:18:26 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,14 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helper/mongo'
+import env from 'dotenv'
 
-app.listen(3333, () => console.log('Listening on port 3333'))
+env.config()
+
+MongoHelper.connect(process.env.MONGO)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+
+    app.listen(3333, () => console.log('Listening on port 3333'))
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
